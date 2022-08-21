21985f65675e641546fd5ed5168d758239ef797d
dluman <dluman@allegheny.edu>
2022-08-21 13:50:39 -0400

    Adding menu command back in for invocation

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -2,6 +2,9 @@ import * as vscode from 'vscode';
 import {termPanel} from "../panels/termPanel";
 
 export function activate(context: vscode.ExtensionContext) {
+  const worldMenu = vscode.commands.registerCommand("world.menu", () => {
+    termPanel.render();
+  })
   termPanel.render();
 }
 
