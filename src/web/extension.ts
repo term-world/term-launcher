1b34c06c2f3d477755da1817c8a38bcd2d51a066
dluman <dluman@allegheny.edu>
2022-08-31 01:34:22 -0400

    Removing stale code

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,8 +1,6 @@
 import * as vscode from 'vscode';
 import {termPanel} from "../panels/termPanel";
 
-
-vscode.ExtensionContext;
 export async function activate(context: vscode.ExtensionContext) {
   const worldMenu = vscode.commands.registerCommand("world.menu", () => {
     termPanel.render(context, context.extensionUri);
